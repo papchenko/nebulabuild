96defab75b48b4ca0d4dfa5c59d1c06327fa1fee
papchenko <nikopapchenko@gmail.com>
2025-02-27 10:47:04 +0200

    v 0.0.1

---
+++ README.md
@@ -0,0 +1,17 @@
+![Nebula.build Logo](logo-full.png)
+
+# Nebula.build
+
+**Uncomment and Use!**
+
+Nebula.build is your ultimate web development assistant, offering pre-built scripts and modular components to enhance your workflow.
+
+## 🚀 Features
+
+- ✅ **Pre-built Scripts** – Speed up your development with ready-to-use code snippets.
+- 🧩 **Modular Components** – Easily integrate and customize components for your projects.
+- ⚡ **Simple & Efficient** – Designed for hassle-free implementation.
+
+## 🔥 Get Started
+
+Start building smarter with **Nebula.build** today!
---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+// show menu
+// import { showMenu } from "./modules/showMenu";
+// showMenu();
+
+// scroll header
+// import { headerScroll } from "./modules/headerScroll";
+// headerScroll();
+
+// show scroll up
+// import { showScrollUp } from "./modules/showScrollUp";
+// showScrollUp();
+
+// slider
+// import { slider } from "./modules/swiper";
+// slider();
+
+// show accordeon
+// import { showAccordeon } from "./modules/showAccordeon";
+// showAccordeon();
+
+// lightbox
+// import { lightboxCustom } from "./modules/lightboxCustom";
+// lightboxCustom();
+
+// gsap
+// import { gsap } from "gsap/gsap-core";
+// gsap();
+
+// scroll reveal
+// import { scrRev } from "./modules/ScrollReveal";
+// scrRev();
